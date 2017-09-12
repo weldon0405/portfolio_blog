8c3c871e110a98d8cdcf097a37791e8a77b0aa21
Weldon <weldon0405@gmail.com>
2017-09-12 19:54:01 -0400

    Created and migrated Post Model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+# portfolio_blog/blog/models.py
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+# from django.db.models import permalink
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    blog_content = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
