9b18023f9e3566b508cd3b3f500f7ee50c56907e
Weldon <weldon0405@gmail.com>
2017-09-12 22:26:02 -0400

    Changed the HTML for the site

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,7 @@
 # portfolio_blog/blog/views.py
 
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
 
-def index(request):
-    return HttpResponse("Hello world. Welcome to the blog index.")
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
