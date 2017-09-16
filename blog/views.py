9a022e46713077521f7729ee3796c928a945733d
Weldon <weldon0405@gmail.com>
2017-09-16 12:11:05 -0400

    save new posts as drafts

--- blog/views.py
+++ blog/views.py
@@ -22,7 +22,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.slug = slugify(post.title)
             post.save()
             return redirect('post_detail', pk=post.pk)
@@ -37,7 +36,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
