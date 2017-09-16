febcf8c4d81a316755234dc207952d3695191d63
Weldon <weldon0405@gmail.com>
2017-09-16 06:35:57 -0400

    Added views to create/edit blog post inside the site.

---
+++ blog/forms.py
@@ -0,0 +1,11 @@
+# portfolio_blog/blog/forms.py
+
+from django import forms
+
+from .models import Post
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'blog_content',)
