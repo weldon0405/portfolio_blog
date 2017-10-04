fdef1964d188251e5ea1afa0ae9ddf3ed0fcc5f9
Weldon <weldon0405@gmail.com>
2017-10-03 21:52:23 -0400

    Add Comment functionality to the blog

--- blog/forms.py
+++ blog/forms.py
@@ -2,10 +2,16 @@
 
 from django import forms
 
-from .models import Post
+from .models import Post, Comment
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('title', 'blog_content',)
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text')
--- portfolio_blog/urls.py
+++ portfolio_blog/urls.py
@@ -17,8 +17,15 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views
+
+from blog.views import post_list
+
 
 urlpatterns = [
+    url(r'^$', post_list, name='index'),
     url(r'^blog/', include('blog.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
 ]
